45c2b2118c6c968c366ebec357dcde3d63d7f725
Roman <zolotarenko46@gmail.com>
2024-06-03 21:44:02 +0300

    Docker-compose

--- web/todo_list/settings.py
+++ web/todo_list/settings.py
@@ -88,10 +88,7 @@ WSGI_APPLICATION = 'todo_list.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / env('DJANGO_DB_NAME'),
-    }
+    'default': env.db()
 }
 
 
